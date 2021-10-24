3336208437b8f9e3c54dcfe45b94c8a29de98c75
Ritiikk <ritikpatel741@gmail.com>
2021-10-24 20:41:19 +0530

    Test-commit

--- js/mains.js
+++ js/mains.js
@@ -1,16 +1,28 @@
-function handleNavItemClick(e) {
-    var clientWidth = document.body.clientWidth;
 
-    if (clientWidth < 920) { 
-        document.getElementsByClassName("navmenu-ul")[0].style.display = "none";
+var hamburger;
+var navMenu;
+var navLink;
+
+function handleOnLoad() {
+    hamburger = document.getElementsByClassName("hamburger")[0];
+    navMenu = document.getElementsByClassName("nav-menu")[0];
+    navLink = document.getElementsByClassName("nav-link");
+    
+    hamburger.addEventListener("click", mobileMenu);
+    console.log(navLink);
+
+    for(var index = 0; index < navLink.length; index++) {
+        navLink[index].addEventListener("click", closeMenu);
     }
+    //navLink.forEach(n => n.addEventListener("click", closeMenu));
 }
 
-window.onresize = function () {
-    var clientWidth = document.body.clientWidth;
-    console.log(clientWidth);
-    
-    if (clientWidth >= 920) {
-        document.getElementsByClassName("navmenu-ul")[0].style.display = "flex";
-    }
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
 }
